b539aa47722c3ec14b45d9148389ca41574c0bed
UselesssMouth <greg.narinsky@yandex.ru>
2019-03-25 01:45:11 +0300

    2.1.1

--- js/script.js
+++ js/script.js
@@ -8,11 +8,18 @@ let zIndexArrSmall = [-26, -28, -30, -32]
 let zIndexArrMIddle = [-18, -20, -22, -24]
 let zIndexArrNeither = [-4, -10, -24]
 
-function changeAnim() {
+let firstTimer = 0
+let secondTimer = 0
+let thirdTimer = 0
+
+
+
+function changeA () {
   let a = zIndexArrBig[getRandomArbitrary(0, zIndexArrBig.length -1)]
 
   $( "#target1" ).css ({
-    "transform": "translateY(" + getRandomArbitrary(-300, 600) + "px)" + "translateX(" + getRandomArbitrary(0, 1600) + "px)" + "scale(" + scaleArr[getRandomArbitrary(0, scaleArr.length - 1)] + ")",
+    "top": getRandomArbitrary(10, 40) + "%",
+    "left": getRandomArbitrary(10, 60) + "%",
     "z-index": a,
   })
 
@@ -23,11 +30,12 @@ function changeAnim() {
 
 }
 
-function changeA() {
-  let b = zIndexArrSmall[getRandomArbitrary(0, zIndexArrSmall.length -1)]
+function changeB() {
+  let a = zIndexArrSmall[getRandomArbitrary(0, zIndexArrSmall.length -1)]
 $( "#target2" ).css ({
-    "transform": "translateY(" + getRandomArbitrary(0, 200) + "px)" + "translateX(" + getRandomArbitrary(-300, 300) + "px)" + "scale(" + scaleArr[getRandomArbitrary(0, scaleArr.length - 1)] + ")",
-    "z-index": b,
+    "top": getRandomArbitrary(40, 70) + "%",
+    "left": getRandomArbitrary(50, 70) + "%",
+    "z-index": a,
   })
 
   $( "#targetB" ).css ({
@@ -35,29 +43,18 @@ $( "#target2" ).css ({
   })
 }
 
-function changeB() {
-  let b = zIndexArrMIddle[getRandomArbitrary(0, zIndexArrMIddle.length -1)]
+function changeC() {
+  let a = zIndexArrMIddle[getRandomArbitrary(0, zIndexArrMIddle.length -1)]
 $( "#target3" ).css ({
-    "transform": "translateY(" + getRandomArbitrary(0, 400) + "px)" + "translateX(" + getRandomArbitrary(100, 300) + "px)" + "scale(" + scaleArr[getRandomArbitrary(0, scaleArr.length - 1)] + ")",
-    "z-index": b,
+  "top": getRandomArbitrary(20, 60) + "%",
+  "left": getRandomArbitrary(-10, 20) + "%",
+    "z-index": a,
   })
 
   $( "#targetC" ).css ({
     "transform": "translateY(" + getRandomArbitrary(0, 25) + "px)" + "translateX(" + getRandomArbitrary(0, 25) + "px)" + "scale(" + scaleArr[getRandomArbitrary(0, scaleArr.length - 1)] + ")",
   })
 }
-
-// function changeC() {
-//   let b = zIndexArrNeither[getRandomArbitrary(0, zIndexArrNeither.length -1)]
-// $( "#target4" ).css ({
-//     "transform": "translateY(" + getRandomArbitrary(-300, 100) + "px)" + "translateX(" + getRandomArbitrary(-100, 100) + "px)" + "scale(" + scaleArr[getRandomArbitrary(0, scaleArr.length - 1)] + ")",
-//     "z-index": b,
-//   })
-//
-//   $( "#targetD" ).css ({
-//     "transform": "translateY(" + getRandomArbitrary(0, 25) + "px)" + "translateX(" + getRandomArbitrary(0, 25) + "px)" + "scale(" + scaleArr[getRandomArbitrary(0, scaleArr.length - 1)] + ")",
-//   })
-// }
 
 function changePositionA() {
   setTimeout(function() {
@@ -77,33 +74,28 @@ function changePositionB() {
   }, 800)
 }
 
-function goChangeAnim() {
-  setTimeout(function() {
-    changeAnim()
-    goChangeAnim()
-  }, 700)
-}
-
 function goChangeA() {
-  setTimeout(function() {
-    changeA()
-    goChangeA()
-  }, 500)
+  let firstTimer = setInterval(changeA, 600)
+  setTimeout(function () {
+    clearInterval(firstTimer)
+}, 3400)
 }
 
 function goChangeB() {
-  setTimeout(function() {
-    changeB()
-    goChangeB()
-  }, 800)
+  let secondTimer = setInterval(changeB, 600)
+  setTimeout(function () {
+    clearInterval(secondTimer)
+}, 3400)
 }
 
-// function goChangeC() {
-//   setTimeout(function() {
-//     changeC()
-//     goChangeC()
-//   }, 600)
-// }
+function goChangeC() {
+  let thirdTimer = setInterval(changeC, 800)
+  setTimeout(function () {
+    clearInterval(thirdTimer)
+}, 3400)
+}
+
+
 
 let firstFrame = anime.timeline({
 
@@ -262,50 +254,224 @@ fourthFrame
   duration: 200,
 })
 
-function logoAnimatio(){
-  let logoAnim = anime.timeline({
 
-  })
-  logoAnim
-  .add({
-    targets: ".good",
-    opacity: 1,
-    duration: 500,
-    easing: "linear"
-  })
-  .add ({
-    targets: ".logo",
-    opacity: 1,
-    duration: 500,
-    easing: "linear"
-  })
-}
 
-function linksAnimation() {
-  anime({
-    targets: ".first, .second, .third",
-    opacity: 1,
-  })
-}
-
-function all() {
+function mainAnimation() {
+  goChangeC()
   goChangeB()
-  goChangeA()
-  // goChangeC()
   changePositionA()
   changePositionB()
-  goChangeAnim()
+  goChangeA()
 }
 
-setTimeout(function() {
-  linksAnimation();
-}, 6000)
+function giantAnimation() {
+
+  $( "#giant-glaz" ).css ({
+    "top": 100 + "px",
+    "transition-duration": 2 + "s",
+  })
+
+  $( "#giant-zrachok" ).css ({
+    "transform": "translateY(" + 150 + "px)" + "translateX(" + 150 + "px)",
+    "transition-duration": 0.1 + "s",
+  })
+}
+
+function giantZrachokAnimation() {
+  $( "#giant-zrachok" ).css ({
+    "transform": "translateY(" + 120 + "px)" + "translateX(" + 250 + "px)",
+    "transition-duration": 0.7 + "s",
+  })
+
+  // setTimeout(function() {
+  //   $( "#giant-zrachok" ).css ({
+  //     "transform": "translateY(" + 120 + "px)" + "translateX(" + 130 + "px)",
+  //     "transition-duration": 0.4 + "s",
+  //   }, 2000)
+  // })
+}
 
 
-setTimeout(function() {
-  logoAnimatio();
-}, 4000)
+
+function cityFalls() {
+  anime({
+    targets: '.img1',
+    height: 500,
+    duration: 3000,
+    delay: 200,
+  })
+  anime({
+    targets: '.img2',
+    height: 227,
+    duration: 3000,
+    delay: 600,
+  })
+  anime({
+    targets: '.img3',
+    height: 360,
+    duration: 3000,
+    delay: 400,
+  })
+  anime({
+    targets: '.img4',
+    height: 367,
+    duration: 3000,
+    delay: 300,
+  })
+  anime({
+    targets: '.img5',
+    height: 520,
+    duration: 2500,
+    delay: 100,
+  })
+  anime({
+    targets: '.img6',
+    height: 412,
+    duration: 2500,
+    delay: 100,
+  })
+  anime({
+    targets: '.img7',
+    height: 282,
+    duration: 2500,
+    delay: 300,
+  })
+  anime({
+    targets: '.img8',
+    height: 210,
+    duration: 2500,
+    delay: 300,
+  })
+  anime({
+    targets: '.img9',
+    height: 148,
+    duration: 2500,
+    delay: 500,
+  })
+  anime({
+    targets: '.img10',
+    height: 181,
+    duration: 2500,
+    delay: 250,
+  })
+  anime({
+    targets: '.img11',
+    height: 246,
+    duration: 2500,
+    delay: 350,
+  })
+  anime({
+    targets: '.img12',
+    height: 356,
+    duration: 2500,
+    delay: 180,
+  })
+  anime({
+    targets: '.img13',
+    height: 399,
+    duration: 2500,
+    delay: 400,
+  })
+  anime({
+    targets: '.img14',
+    height: 261,
+    duration: 2500,
+    delay: 150,
+  })
+  anime({
+    targets: '.img15',
+    height: 104,
+    duration: 2500,
+    delay: 350,
+  })
+  anime({
+    targets: '.img16',
+    height: 216,
+    duration: 2500,
+    delay: 400,
+  })
+  anime({
+    targets: '.img17',
+    height: 244,
+    duration: 2500,
+    delay: 300,
+  })
+  anime({
+    targets: '.img18',
+    height: 141,
+    duration: 2500,
+    delay: 12,
+  })
+}
+
+function runAway() {
+
+  setTimeout(function() {
+    $( "#target1" ).css ({
+      "top": 20 + "%",
+      "left": 80 + "%",
+      "transition-duration": 1 + "s",
+    })
+
+  }, 300)
+
+
+  $( "#target2" ).css ({
+    "top": 10 + "%",
+    "left": 80 + "%",
+    "transition-duration": 2 + "s",
+
+  })
+
+  $( "#target3" ).css ({
+    "top": 18 + "%",
+    "left": 72 + "%",
+    "transition-duration": 2 + "s",
+  })
+
+    // $( "#targetA" ).css ({
+    //   "transform": "translateY(" + 50 + "px)" + "translateX(" + 50 + "px)",
+    //   "transition-duration": 0.4 + "s",
+    // })
+
+    setTimeout(function() {
+      $( "#targetA" ).css ({
+        "transform": "translateY(" + 50 + "px)" + "translateX(" + 15 + "px)",
+        "transition-duration": 0.4 + "s",
+      })
+    }, 600)
+
+    setTimeout(function() {
+      $( "#targetB" ).css ({
+        "transform": "translateY(" + 20 + "px)" + "translateX(" + 5 + "px)",
+        "transition-duration": 0.4 + "s",
+      })
+    }, 600)
+
+    setTimeout(function() {
+      $( "#targetC" ).css ({
+        "transform": "translateY(" + 20 + "px)" + "translateX(" + 5 + "px)",
+        "transition-duration": 0.4 + "s",
+      })
+    }, 600)
+
+}
+  setTimeout(function() {
+  mainAnimation();
+}, 1800)
 
 setTimeout(function() {
-  all();
-}, 2000)
+  giantAnimation()
+}, 4600)
+
+setTimeout(function() {
+    cityFalls()
+}, 5200)
+
+setTimeout(function() {
+  runAway()
+}, 5200)
+
+setTimeout(function() {
+  giantZrachokAnimation()
+}, 6200)
